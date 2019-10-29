64b2729825febb8ddb8c6b5abbec502600be9c35
Vincent Tran <VincentTran197120@gmail.com>
2019-10-29 17:08:05 -0500

    Fixed https issue

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs'
 //http://jservice.io/popular/18418
 export class DataService {
 
-  private baseURL:string = "http://jservice.io"
+  private baseURL:string = "http://www.jservice.io"
   public dataResult: any;
   public currentIndex: string;
   public category:any = [];
