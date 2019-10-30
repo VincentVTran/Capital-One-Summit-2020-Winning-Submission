0aa2590ceec72712f084d44e9777052a7205be47
Vincent Tran <VincentTran197120@gmail.com>
2019-10-30 14:33:22 -0500

    Added cors anywhere

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs'
 //http://jservice.io/popular/18418
 export class DataService {
 
-  private baseURL:string = "http://www.jservice.io"
+  private baseURL:string = "https://cors-anywhere.herokuapp.com/http://jservice.io/"
   public dataResult: any;
   public currentIndex: string;
   public currentIndex2: string;
